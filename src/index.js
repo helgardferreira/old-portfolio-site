eda7df9de59fb8a86903468e40b0644aa450808d
Helgard Ferreira <hrfer3@gmail.com>
2019-08-20 19:39:16 +0200

    Remove index.css import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'typeface-roboto';
 import 'font-awesome/css/font-awesome.css';
 // import * as serviceWorker from './serviceWorker';

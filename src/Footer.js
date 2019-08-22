21bfa406df199b69c84a9333939b1e0ce667a6fe
Helgard Ferreira <hrfer3@gmail.com>
2019-08-22 19:16:03 +0200

    Update Header

--- src/Footer.js
+++ src/Footer.js
@@ -3,14 +3,15 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(theme => ({
+  footer: {
+    backgroundColor: theme.palette.tertiary.dark,
+    color: theme.palette.text.main,
+    padding: theme.spacing(3),
+  },
+}));
+
 export default function Footer() {
-  const useStyles = makeStyles(theme => ({
-    footer: {
-      backgroundColor: theme.palette.tertiary.dark,
-      color: theme.palette.text.main,
-      padding: theme.spacing(3),
-    },
-  }));
   const classes = useStyles();
 
   return (
--- src/Header.js
+++ src/Header.js
@@ -3,37 +3,77 @@ import { navigate } from '@reach/router';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import ProteusLogoCircle from './logo-circle.svg';
+import Icon from '@material-ui/core/Icon';
+import grey from '@material-ui/core/colors/grey';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+    height: theme.spacing(7),
+    cursor: 'pointer',
+  },
+  appBar: {
+    backgroundColor: theme.palette.tertiary.dark,
+  },
+}));
+
+const HeaderIcon = withStyles(theme => ({
+  root: {
+    cursor: 'pointer',
+    fontSize: theme.spacing(4),
+    marginLeft: theme.spacing(3),
+    color: 'white',
+    '&:hover': {
+      color: grey[400],
+    },
+  },
+}))(Icon);
 
 export default function Header() {
-  const useStyles = makeStyles(theme => ({
-    icon: {
-      marginRight: theme.spacing(2),
-      height: theme.spacing(7),
-      cursor: 'pointer',
-    },
-    appBar: {
-      backgroundColor: theme.palette.tertiary.dark,
-    },
-  }));
   const classes = useStyles();
 
   return (
-    <header>
+    <header className={classes.root}>
       <AppBar position="relative" className={classes.appBar}>
         <Toolbar>
           <img
-            onClick={() => {
-              navigate('/');
-            }}
+            onClick={() => navigate('/')}
             src={ProteusLogoCircle}
             alt="Logo"
             className={classes.icon}
           />
-          <Typography variant="h6" color="inherit" noWrap>
+
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
             Home
           </Typography>
+
+          <a
+            href="https://www.linkedin.com/in/helgard-richard-ferreira/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <HeaderIcon className={`fa fa-linkedin`} />
+          </a>
+          <a
+            href="https://github.com/helgardferreira"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <HeaderIcon className={`fa fa-github-square`} />
+          </a>
         </Toolbar>
       </AppBar>
     </header>
